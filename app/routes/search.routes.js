af335c8ac4274ce0d9377208bb70dd95653368f0
Rohit Patil <rohitpatil07122001@gmail.com>
2021-10-16 20:10:18 +0530

    New route message
    Signed-off-by: Rohit Patil <rohitpatil07122001@gmail.com>

--- app/routes/search.routes.js
+++ app/routes/search.routes.js
@@ -3,5 +3,8 @@ const router = Router();
 import searchControllers from '../controllers/search.controllers.js';
 
 router.get('/', searchControllers.searchpattern);
+router.get('/message', (req,res)=>{
+    res.json("THIS IS A NEW ADDITTION MESSAGE TO VERIFY GITHUB ACTONS");
+})
 
 export default router;
